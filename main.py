d48f1fc9288cfbb1ff0815678fe6f302f3f68019
kido2k3 <hoangphongvip2003@gmail.com>
2024-02-21 17:33:04 +0700

    add require, ai model, finish send data

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import random
 import time
 from my_adafruit_connection import AdafruitConnection
-
+from my_ai import DetectPersonModel
 
 my_server = AdafruitConnection()
-counter = 10
-
+my_model = DetectPersonModel()
+timer = 10
+timer_ai = 5
+pre_res = None
 while True:
-    counter = counter-1
-    if counter <= 0:
-        counter = 10
+    timer -= 1
+    timer_ai -= 1
+    if timer <= 0:
+        timer = 10
+        # send a random data every 10s
         value = random.randint(0, 1)
         print(f'Publishing {value} to {my_server.AIO_FEED_NAMES[0]}.')
         my_server.client.publish(
-            f'{my_server.AIO_USERNAME}/feeds/{my_server.AIO_FEED_NAMES[0]}', value)
+            f'{my_server.AIO_USERNAME}/feeds/{my_server.AIO_FEED_NAMES[0]}',
+            value)
+    if timer_ai < 0:
+        timer_ai = 5
+        # send the result of my AI model
+        res, score = my_model.detect_person()
+        # send the data only if the score high and not same result
+        if score > 0.8 and pre_res != res:
+            print(f'Publishing {res} to {my_server.AIO_FEED_NAMES[5]}.')
+            my_server.client.publish(
+                f'{my_server.AIO_USERNAME}/feeds/{my_server.AIO_FEED_NAMES[5]}',
+                res)
+            pre_res = res
     time.sleep(1)
+    # ESC is pressed
+    if my_model.exit() == 1:
+        break
--- my_adafruit_connection.py
+++ my_adafruit_connection.py
@@ -6,9 +6,9 @@ import sys
 
 class AdafruitConnection:
     # Some config paras
-    AIO_FEED_NAMES = ['iot-pump', 'iot-led']
+    AIO_FEED_NAMES = ['iot-pump', 'iot-led', 'iot-brightness','iot-humidity','iot-temperature','iot-ai']
     AIO_USERNAME = 'kido2k3'
-    AIO_KEY = 'aio_kHTu8402XLf274WB8GmdF4b2UKhA'
+    AIO_KEY = 'aio_jaOB74D9fmmRysu5Tsjd5m98hin3'
     client = None
 
     # The callback for when the client receives a CONN_ACK response from the server.
